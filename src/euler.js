8d85d1ced4738a2926c6758867ff06c6b9067beb
Kim Brannian <brannian@gmail.com>
2022-02-02 10:07:59 -0800

    solve all of the problems
    Co-authored-by: Frank Proulx <frankproulx7@gmail.com>

--- src/euler.js
+++ src/euler.js
@@ -1,42 +1,6 @@
 
-// This function stores our state.
-
-// export const storeState = () => {
-//   let currentState = {};
-//   return (stateChangeFunction = state => state) => {
-//     const newState = stateChangeFunction(currentState);
-//     currentState = {...newState};
-//     return newState;
-//   }
-// }
-
-// export const stateControl = storeState();
-
-// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
-
-// export const changeState = (prop) => {
-//   return (value) => {
-//     return (state) => ({
-//       ...state, 
-//       [prop] : (state[prop] || 0) + value
-//     })
-//   }
-// }
-
-// export const changeState = (prop1, prop2) => {
-//   return (value1, value2) => {
-//     return (state) => ({
-//       ...state,  
-//       [prop1] : (state[prop1] || 0) + value1,
-//       [prop2] : (state[prop2] || 0) + value2
-//     })
-//   }
-// }
-
-// We create four functions using our function factory. We could easily create many more.
-
 function euler(number) {
-  counter = 0
+  let counter = 0
   for (let i = 1; i < number; i++) {
     if ((i % 3 == 0) || (i % 5 == 0)) {
       counter += i;
@@ -55,15 +19,29 @@ const euler = (num) => {
   }
 };
 
-const incrementCounter = (counter) => {
-  // This is the termination condition.
-  if (isNaN(counter)) {
-    return;
+// const incrementCounter = (counter) => {
+//   // This is the termination condition.
+//   if (isNaN(counter)) {
+//     return;
+//   }
+//   if (counter >= 3) {
+//     return counter;
+//   } else {
+//     console.log(counter);
+//     return incrementCounter(counter + 1);
+//   }
+// }
+
+function fibonacciEven(num){
+  let fib = [1,2];
+  let sum = 0
+  while(fib[fib.length-1]<num) {
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
   }
-  if (counter >= 3) {
-    return counter;
-  } else {
-    console.log(counter);
-    return incrementCounter(counter + 1);
-  }
+  fib.forEach(function(element) { 
+    if(element % 2 == 0){
+      sum += element;
+    }
+  });
+  return sum; 
 }
